3f23ad24d14268a9ad14a3bd19aa554af49c794d
Alexandre Cardoso <alecar2007@gmail.com>
2024-12-02 23:37:38 -0300

    chore: phpstan-config

--- src/core/helpers/constants.php
+++ src/core/helpers/constants.php
@@ -1,5 +1,10 @@
 <?php
 
+if (!isset($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'])) {
+  $_SERVER['REQUEST_URI'] = '/';
+  $_SERVER['REQUEST_METHOD'] = 'GET';
+}
+
 define('BASE_PATH', dirname(__DIR__, 2));
 define('BASE_URL', 'http://localhost:8000');
 define('REQUEST_URI', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
